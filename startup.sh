6dc821dfb23f32733cfcc57fbdb27f8e2e4fa4ea
lc <lichon@foxmail.com>
2022-06-02 10:37:07 +0800

    remove random page

--- startup.sh
+++ startup.sh
@@ -3,15 +3,6 @@
 envsubst < /root/config.json.tp > /root/config.json
 cat /root/config.json
 
-# get random page from wikipedia
-if [[ -e "/root/html/index.html" ]]; then
-    echo "index.html exsit, skip genreate index page"
-else
-    randomurl=$(curl -L 'https://en.wikipedia.org/api/rest_v1/page/random/summary' | jq -r '.content_urls.desktop.page')
-    echo $randomurl
-    curl "$randomurl" -o /root/html/index.html
-fi
-
 # Run V2Ray
 if [[ $TUNNEL_TOKEN ]]; then
 echo 'has tunnel token, run cloudflared tunnel'
